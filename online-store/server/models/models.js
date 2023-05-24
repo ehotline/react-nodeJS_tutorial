c5c425aaa3adb2d34d40f36f48968862e260b9e7
Дмитрий <deslink123@gmail.com>
2023-05-24 20:04:25 +0300

    Получение устройств, фильтрация, пагинация

--- online-store/server/models/models.js
+++ online-store/server/models/models.js
@@ -70,11 +70,11 @@ Rating.belongsTo(Device)
 Device.hasMany(BasketDevice)
 BasketDevice.belongsTo(Device)
 
-Device.hasMany(DeviceInfo)
+Device.hasMany(DeviceInfo, { as: 'info' })
 DeviceInfo.belongsTo(Device)
 
-Type.belongsToMany(Brand, {through: TypeBrand })
-Brand.belongsToMany(Type, {through: TypeBrand })
+Type.belongsToMany(Brand, { through: TypeBrand })
+Brand.belongsToMany(Type, { through: TypeBrand })
 
 module.exports = {
     User,
